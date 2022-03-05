5bff8114cc586fc870844819e8290d55b5f35977
kamax0123 <angelfirst930124@gmail.com>
2022-03-05 14:21:31 +0300

    change of the test folder

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ const app = require("../index");
 
 describe("GET /", () => {
     it("App working correctly", (done) => {
-        request(app).get("/").expect("App Working", done);
+        request(app).get("/").expect("App Working!", done);
     })
 });
